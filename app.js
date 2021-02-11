f94c1a83009a627f1ce68539057d1bde59ec3d15
Rumiani <maziar9170@gmail.com>
2021-02-11 12:09:37 +0330

    Add files via upload

---
+++ app.js
@@ -0,0 +1,91 @@
+
+var xnum;
+var ynum;
+var znum;
+//////////add number
+var addnumbtn=document.querySelector('.addnumbtn');
+
+
+
+addnumbtn.addEventListener("keyup", enterbtn);
+
+function enterbtn(event) {
+    if (event.keyCode === 13) {
+    event.preventDefault();
+    document.querySelector('.addnumbtn').click();
+   }
+ }
+
+
+ addnumbtn.addEventListener('click',addanumber);
+
+ function hey(){
+     alert("hey")
+ }
+
+
+
+
+
+
+
+var myArray=[];
+
+function addanumber(){
+     var newnum=document.querySelector('.inputnum').value;
+     newnum=Number(newnum);
+     if (newnum>45 || newnum<3){
+      alert("your number sould be between 3 and 45")
+         }
+     else{
+         myArray.push(newnum);
+         }
+         
+console.log(myArray)
+console.log(typeof myArray)
+console.log(typeof newnum)
+//show the numbers
+demo()
+function demo(){
+const demonumbers=document.querySelector(".demonumbers");
+demonumbers.innerHTML=myArray;
+}
+}
+
+//add the goal
+// myArray=[3,4,5,6,7,8,9,10,11,12,13,14,15];
+
+var entergoalnumber=document.querySelector('.goalnumbtn');
+entergoalnumber.addEventListener('click',addgoal);
+// randomaray();
+function addgoal(){
+console.log(myArray)
+    
+let goalnum=document.querySelector('.inputgoal').value;
+goalnum=Number(goalnum);
+let sum=goalnum;
+ randomaray ();
+function randomaray (){
+ xnum = myArray[Math.floor(Math.random()*myArray.length)];
+//   console.log(xnum);
+  ynum = myArray[Math.floor(Math.random()*myArray.length)];
+//   console.log(ynum);
+   znum = myArray[Math.floor(Math.random()*myArray.length)];
+//   console.log(znum);
+ sum=xnum+ynum+znum
+ while(sum!=goalnum){
+    randomaray();   
+}
+}
+demogoal()
+function demogoal(){
+    
+const demonumbers=document.querySelector(".demogoal");
+demonumbers.innerHTML=`${xnum}+${ynum}+${znum}=${sum}`;
+}
+}
+window.onerror = function(msg, url, linenumber) {
+    // alert('Error message: '+msg+'\nURL: '+url+'\nLine Number: '+linenumber);
+    alert("you can not choose this number because it is too small or too big")
+    return true;
+}
